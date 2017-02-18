2b84e690f6b7637143a25a52c39ddd07746b54ac
WarmUpTill <WarmUpTill@users.noreply.github.com>
2017-02-19 00:31:30 +0100

    Rewrite based on OBS Media Source
    Fixes alpha channel not being displayed for some inputs.
    Fixes timing issues if input fps and OBS fps were different.
    Improves performance.

--- transition_stinger.c
+++ transition_stinger.c
@@ -6,6 +6,13 @@
 #include <libswscale/swscale.h>
 #include <libavutil/pixfmt.h>
 
+
+
+#include "obs-ffmpeg-compat.h"
+#include "obs-ffmpeg-formats.h"
+
+#include <libff/ff-demuxer.h>
+
 //#include <windows.h>
 //
 //
@@ -16,6 +23,10 @@
 //
 //	OutputDebugStringA(szBuff);
 //}
+
+//av_log_set_callback(my_log_callback);
+//av_log_set_level(AV_LOG_VERBOSE);
+
 
 
 struct stinger_info {
@@ -38,14 +49,24 @@ struct stinger_info {
 	size_t curFrame;
 	size_t numberOfFrames;
 
-	AVFormatContext *pFormatCtx;
-	AVCodecContext *pCodecCtxOrig;
-	AVCodecContext *pCodecCtx;
-	AVCodec *pCodec;
-	AVFrame *pFrame;
-	AVFrame *pFrameRGB;
+	struct ff_demuxer *demuxer;
 	struct SwsContext *sws_ctx;
-	int videoStream;
+	int sws_width;
+	int sws_height;
+	enum AVPixelFormat sws_format;
+	uint8_t *sws_data;
+	int sws_linesize;
+
+	enum AVDiscard frame_drop;
+	enum video_range_type range;
+	int audio_buffer_size;
+	int video_buffer_size;
+	bool is_advanced;
+	bool is_looping;
+	bool is_forcing_scale;
+	bool is_hw_decoding;
+	bool is_clear_on_media_end;
+	bool restart_on_activate;
 	
 };
 
@@ -55,9 +76,252 @@ static const char *stinger_get_name(void *type_data)
 	return obs_module_text("StingerTransition");
 }
 
+bool update_sws_context(struct stinger_info *s, AVFrame *frame)
+{
+	if (frame->width != s->sws_width
+		|| frame->height != s->sws_height
+		|| frame->format != s->sws_format) {
+		if (s->sws_ctx != NULL)
+			sws_freeContext(s->sws_ctx);
+
+		if (frame->width <= 0 || frame->height <= 0) {
+			blog(LOG_ERROR, "unable to create a sws "
+				"context that has a width(%d) or "
+				"height(%d) of zero.", frame->width,
+				frame->height);
+			goto fail;
+		}
+
+		s->sws_ctx = sws_getContext(
+			frame->width,
+			frame->height,
+			frame->format,
+			frame->width,
+			frame->height,
+			AV_PIX_FMT_BGRA,
+			SWS_BILINEAR,
+			NULL, NULL, NULL);
+
+		if (s->sws_ctx == NULL) {
+			blog(LOG_ERROR, "unable to create sws "
+				"context with src{w:%d,h:%d,f:%d}->"
+				"dst{w:%d,h:%d,f:%d}",
+				frame->width, frame->height,
+				frame->format, frame->width,
+				frame->height, AV_PIX_FMT_BGRA);
+			goto fail;
+
+		}
+
+		if (s->sws_data != NULL)
+			bfree(s->sws_data);
+		s->sws_data = bzalloc(frame->width * frame->height * 4);
+		if (s->sws_data == NULL) {
+			blog(LOG_ERROR, "unable to allocate sws "
+				"pixel data with size %d",
+				frame->width * frame->height * 4);
+			goto fail;
+		}
+
+		s->sws_linesize = frame->width * 4;
+		s->sws_width = frame->width;
+		s->sws_height = frame->height;
+		s->sws_format = frame->format;
+	}
+
+	return true;
+
+fail:
+	if (s->sws_ctx != NULL)
+		sws_freeContext(s->sws_ctx);
+	s->sws_ctx = NULL;
+
+	if (s->sws_data)
+		bfree(s->sws_data);
+	s->sws_data = NULL;
+
+	s->sws_linesize = 0;
+	s->sws_width = 0;
+	s->sws_height = 0;
+	s->sws_format = 0;
+
+	return false;
+}
+
+static void setNextFrameTexture(struct stinger_info *stinger, AVFrame *frame)
+{
+	if (!stinger || !stinger->validInput)
+		return;
+
+	obs_enter_graphics();
+	gs_texture_destroy(stinger->stinger_texture);
+	stinger->stinger_texture = gs_texture_create(
+		frame->width, frame->height, 5, 1, //5 is AV_PIX_FMT_BGRA
+		(const uint8_t**)&frame->data, 0);
+	obs_leave_graphics();
+
+	stinger->curFrame++;
+}
+
+
+static bool video_frame_scale(struct ff_frame *frame,
+struct stinger_info *s, AVFrame *pFrame)
+{
+	if (!update_sws_context(s, frame->frame))
+		return false;
+
+	sws_scale(
+		s->sws_ctx,
+		(uint8_t const *const *)frame->frame->data,
+		frame->frame->linesize,
+		0,
+		frame->frame->height,
+		&s->sws_data,
+		&s->sws_linesize
+		);
+
+	pFrame->data[0] = s->sws_data;
+	pFrame->linesize[0] = s->sws_linesize;
+	pFrame->format = AV_PIX_FMT_BGRA;
+
+	setNextFrameTexture(s, pFrame);
+
+	return true;
+}
+static bool video_frame_hwaccel(struct ff_frame *frame,
+struct stinger_info *s, AVFrame *pFrame)
+{
+	// 4th plane is pixelbuf reference for mac
+	for (int i = 0; i < 3; i++) {
+		pFrame->data[i] = frame->frame->data[i];
+		pFrame->linesize[i] = frame->frame->linesize[i];
+	}
+
+	//if (!set_obs_frame_colorprops(frame, s, obs_frame))
+	//	return false;
+
+	setNextFrameTexture(s, pFrame);
+	return true;
+}
+
+static bool video_frame_direct(struct ff_frame *frame,
+struct stinger_info *s, AVFrame *pFrame)
+{
+	int i;
+
+	for (i = 0; i < MAX_AV_PLANES; i++) {
+		pFrame->data[i] = frame->frame->data[i];
+		pFrame->linesize[i] = frame->frame->linesize[i];
+	}
+
+	//if (!set_obs_frame_colorprops(frame, s, obs_frame))
+	//	return false;
+
+	setNextFrameTexture(s, pFrame);
+	return true;
+}
+
+static bool video_frame(struct ff_frame *frame, void *opaque)
+{
+	struct stinger_info *s = opaque;
+
+	// Media ended
+	if (frame == NULL) {
+		return true;
+	}
+	AVFrame *pFrame = NULL;
+	pFrame = av_frame_alloc();
+
+	pFrame->format = AV_PIX_FMT_BGRA;
+	pFrame->width = frame->frame->width;
+	pFrame->height = frame->frame->height;
+
+	av_image_alloc(pFrame->data,
+		pFrame->linesize,
+		pFrame->width,
+		pFrame->height,
+		AV_PIX_FMT_BGRA,
+		16
+		);
+
+	enum video_format format =
+		ffmpeg_to_obs_video_format(frame->frame->format);
+
+	if (s->is_forcing_scale || format == VIDEO_FORMAT_NONE)
+		return video_frame_scale(frame, s, pFrame);
+	else if (s->is_hw_decoding)
+		return video_frame_hwaccel(frame, s, pFrame);
+	else
+		return video_frame_direct(frame, s, pFrame);
+}
+
+//static bool audio_frame(struct ff_frame *frame, void *opaque)
+//{
+//	struct ffmpeg_source *s = opaque;
+//
+//	struct obs_source_audio audio_data = { 0 };
+//
+//	uint64_t pts;
+//
+//	// Media ended
+//	if (frame == NULL || frame->frame == NULL)
+//		return true;
+//
+//	pts = (uint64_t)(frame->pts * 1000000000.0L);
+//
+//	int channels = av_frame_get_channels(frame->frame);
+//
+//	for (int i = 0; i < channels; i++)
+//		audio_data.data[i] = frame->frame->data[i];
+//
+//	audio_data.samples_per_sec = frame->frame->sample_rate;
+//	audio_data.frames = frame->frame->nb_samples;
+//	audio_data.timestamp = pts;
+//	audio_data.format =
+//		convert_ffmpeg_sample_format(frame->frame->format);
+//	audio_data.speakers = channels;
+//
+//	obs_source_output_audio(s->source, &audio_data);
+//
+//	return true;
+//}
+
+
+
+static void ffmpeg_source_start(struct stinger_info *s)
+{
+	if (s->demuxer != NULL)
+		ff_demuxer_free(s->demuxer);
+
+	s->demuxer = ff_demuxer_init();
+	s->demuxer->options.is_hw_decoding = s->is_hw_decoding;
+	s->demuxer->options.is_looping = false;
+
+	ff_demuxer_set_callbacks(&s->demuxer->video_callbacks,
+		video_frame, NULL,
+		NULL, NULL, NULL, s);
+
+	//ff_demuxer_set_callbacks(&s->demuxer->audio_callbacks,
+	//	audio_frame, NULL,
+	//	NULL, NULL, NULL, s);
+
+	ff_demuxer_open(s->demuxer, s->path, NULL);
+}
+
+
+
+
 static void stinger_update(void *data, obs_data_t *settings)
 {
 	struct stinger_info *stinger = data;
+
+
+	bool is_local_file = obs_data_get_bool(settings, "is_local_file");
+	bool is_advanced = obs_data_get_bool(settings, "advanced");
+
+	stinger->is_hw_decoding = obs_data_get_bool(settings, "hw_decode");
+	stinger->is_forcing_scale = true;
+	stinger->lastTime = 1.0f; //to make sure it plays on first scene change
 
 	stinger->path = obs_data_get_string(settings, "stingerPath");
 	stinger->cutFrame = obs_data_get_int(settings, "cutFrame");
@@ -69,95 +333,43 @@ static void stinger_update(void *data, obs_data_t *settings)
 		stinger->numberOfFrames = numberOfFrames;
 		obs_data_set_int(settings, "numberOfFrames", numberOfFrames);
 
-		sws_freeContext(stinger->sws_ctx);
+		AVCodecContext *pCodecCtx = NULL;
+		AVCodec *pCodec = NULL;
+		AVFormatContext *pFormatCtx = NULL;
 
-		av_free(stinger->pFrame);
-		av_free(stinger->pFrameRGB);
-
-		avcodec_close(stinger->pCodecCtx);
-		avcodec_close(stinger->pCodecCtxOrig);
-
-		avformat_close_input(&stinger->pFormatCtx);
-
-		stinger->pFormatCtx = NULL;
-
-		if (avformat_open_input(&stinger->pFormatCtx, stinger->path, NULL, NULL) != 0)
+		if (avformat_open_input(&pFormatCtx, stinger->path, NULL, NULL) != 0)
 		{
 			stinger->validInput = false;
 			return; // Couldn't open file
 		}
 
-		if (avformat_find_stream_info(stinger->pFormatCtx, NULL)<0)
+		if (avformat_find_stream_info(pFormatCtx, NULL)<0)
 		{
 			stinger->validInput = false;
 			return; // Couldn't find stream information
 		}
 
-		int i;
-		stinger->pCodecCtxOrig = NULL;
-		stinger->pCodecCtx = NULL;
-		stinger->pCodec = NULL;
-		stinger->videoStream = 0;
+		int videoStream = av_find_best_stream(pFormatCtx, AVMEDIA_TYPE_VIDEO, -1, -1, pCodec, 0);
 
-		stinger->videoStream = av_find_best_stream(stinger->pFormatCtx, AVMEDIA_TYPE_VIDEO, -1, -1, stinger->pCodec, 0);
+		pCodecCtx = pFormatCtx->streams[videoStream]->codec;
 
-		stinger->pCodecCtxOrig = stinger->pFormatCtx->streams[stinger->videoStream]->codec;
+		pCodec = avcodec_find_decoder(pCodecCtx->codec_id);
 
-		stinger->pCodec = avcodec_find_decoder(stinger->pCodecCtxOrig->codec_id);
-		if (stinger->pCodec == NULL)
+		if (pCodec == NULL)
 		{
 			stinger->validInput = false;
 			return; // Codec not found
 		}
-		stinger->pCodecCtx = avcodec_alloc_context3(stinger->pCodec);
-		if (avcodec_copy_context(stinger->pCodecCtx, stinger->pCodecCtxOrig) != 0)
-		{
-			stinger->validInput = false;
-			return; // Error copying codec context
-		}
-		// Open codec
-		if (avcodec_open2(stinger->pCodecCtx, stinger->pCodec, NULL)<0)
-		{
-			stinger->validInput = false;
-			return; // Could not open codec
-		}
 
-		stinger->pFrame = NULL;
-		stinger->pFrameRGB = NULL;
+		double duration = 
+			(double)stinger->numberOfFrames / 
+			(double)pCodecCtx->framerate.num * 
+			(double)pCodecCtx->framerate.den * 
+			1000.0;
 
-		stinger->pFrame = av_frame_alloc();
-		stinger->pFrameRGB = av_frame_alloc();
 
-		stinger->sws_ctx = NULL;
-
-		// initialize SWS context for software scaling
-		stinger->sws_ctx = sws_getContext(stinger->pCodecCtx->width,
-			stinger->pCodecCtx->height,
-			stinger->pCodecCtx->pix_fmt,
-			stinger->pCodecCtx->width,
-			stinger->pCodecCtx->height,
-			AV_PIX_FMT_BGRA,
-			SWS_BILINEAR,
-			NULL,
-			NULL,
-			NULL
-			);
-
-		stinger->pFrameRGB->format = AV_PIX_FMT_BGRA;
-		stinger->pFrameRGB->width = stinger->pCodecCtx->width;
-		stinger->pFrameRGB->height = stinger->pCodecCtx->height;
-
-		av_image_alloc(stinger->pFrameRGB->data,
-			stinger->pFrameRGB->linesize,
-			stinger->pCodecCtx->width,
-			stinger->pCodecCtx->height,
-			AV_PIX_FMT_BGRA,
-			16
-			);
-
-		stinger->curFrame = 0;
-
-		double duration = (double)stinger->numberOfFrames / (double)stinger->pCodecCtxOrig->framerate.num * (double)stinger->pCodecCtxOrig->framerate.den * 1000.0;
+		avcodec_close(pCodecCtx);
+		avformat_close_input(&pFormatCtx);
 
 		obs_transition_enable_fixed(stinger->source, true,
 			(uint32_t)duration);
@@ -227,15 +439,13 @@ static void stinger_destroy(void *data)
 {
 	struct stinger_info *stinger = data;
 
-	sws_freeContext(stinger->sws_ctx);
 
-	av_free(stinger->pFrame);
-	av_free(stinger->pFrameRGB);
+	if (stinger->demuxer)
+		ff_demuxer_free(stinger->demuxer);
 
-	avcodec_close(stinger->pCodecCtx);
-	avcodec_close(stinger->pCodecCtxOrig);
-
-	avformat_close_input(&stinger->pFormatCtx);
+	if (stinger->sws_ctx != NULL)
+		sws_freeContext(stinger->sws_ctx);
+	bfree(stinger->sws_data);
 
 	obs_enter_graphics();
 	if (!stinger->validInput)
@@ -249,65 +459,17 @@ static void stinger_destroy(void *data)
 	bfree(stinger);
 }
 
-static void setNextFrameTexture(struct stinger_info *stinger)
-{
-	if (!stinger || !stinger->validInput)
-		return;
-
-	//av_log_set_callback(my_log_callback);
-	//av_log_set_level(AV_LOG_VERBOSE);
-
-	int frameFinished;
-	AVPacket packet;
-	int i = 0;
-
-	if (av_read_frame(stinger->pFormatCtx, &packet) >= 0)
-	{
-		if (packet.stream_index == stinger->videoStream)
-		{
-			avcodec_decode_video2(stinger->pCodecCtx, stinger->pFrame, &frameFinished, &packet);
-
-			if (frameFinished) {
-
-				// Convert the image from its native format to BGRA
-				sws_scale(stinger->sws_ctx, (uint8_t const * const *)stinger->pFrame->data,
-					stinger->pFrame->linesize, 0, stinger->pCodecCtx->height,
-					stinger->pFrameRGB->data, stinger->pFrameRGB->linesize);
-
-				obs_enter_graphics();
-				gs_texture_destroy(stinger->stinger_texture);
-				stinger->stinger_texture = gs_texture_create(
-					stinger->pFrameRGB->width, stinger->pFrameRGB->height, 5, 1, //5 is AV_PIX_FMT_BGRA
-					(const uint8_t**)&stinger->pFrameRGB->data, 0);
-				obs_leave_graphics();
-			}
-		}
-		av_free_packet(&packet);
-	}
-	stinger->curFrame++;
-}
-
-static void seekBeginning(struct stinger_info *stinger){
-	if (!stinger->validInput)
-		return;
-	av_seek_frame(stinger->pFormatCtx,
-		stinger->videoStream,
-		0, AVSEEK_FLAG_BACKWARD);
-}
-
 static void stinger_callback(void *data, gs_texture_t *a, gs_texture_t *b,
 		float t, uint32_t cx, uint32_t cy)
 {
 	struct stinger_info *stinger = data;
 
-	if (t - stinger->lastTime < 0.0f)
+	if (t - stinger->lastTime < 0.0f) //new scene change
 	{
-		seekBeginning(stinger);
+		ffmpeg_source_start(stinger);
 		stinger->curFrame = 0;
 		stinger->lastTime = t;
 	}
-
-	setNextFrameTexture(stinger); //increases curFrame
 	
 	if (stinger->curFrame < stinger->cutFrame - 1)
 		gs_effect_set_texture(stinger->ep_a_tex, a);
@@ -355,9 +517,6 @@ static bool stinger_audio_render(void *data, uint64_t *ts_out,
 static int checkInputFile(const char* file)
 {
 	int numberOfFrames = 0;
-
-	//av_log_set_callback(my_log_callback);
-	//av_log_set_level(AV_LOG_VERBOSE);
 
 	if (!file)
 		return 0;
@@ -474,6 +633,8 @@ static obs_properties_t *stinger_properties(void *data)
 
 	obs_property_t *pathProp = obs_properties_add_path(ppts, "stingerPath", "Path to stinger video", OBS_PATH_FILE, "", "");
 	obs_property_set_modified_callback(pathProp, stingerPathModified);
+	obs_properties_add_bool(ppts, "hw_decode",
+		obs_module_text("HardwareDecode"));
 
 	return ppts;
 }
@@ -485,6 +646,9 @@ static void stinger_defaults(obs_data_t *settings)
 	obs_data_set_default_int(settings, "numberOfFrames", 1);
 	obs_data_set_default_bool(settings, "validInput", false);
 	obs_data_set_default_string(settings, "stingerPath", "");
+#if defined(_WIN32)
+	obs_data_set_default_bool(settings, "hw_decode", true);
+#endif
 }
 
 
@@ -500,5 +664,3 @@ struct obs_source_info stinger_transition = {
 	.get_properties = stinger_properties,
 	.get_defaults = stinger_defaults
 };
-
-//TODO add float time cut slider
